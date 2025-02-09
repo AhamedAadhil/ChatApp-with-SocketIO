d80130d8f68ce7eb37e7a59919a772c3849161ec
Ahamed Aathil <ahamedaathil.5@gmail.com>
2025-02-09 14:19:12 +0500

    backend: db config complete
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

---
+++ backend/src/controllers/auth.controllers.js
@@ -0,0 +1,11 @@
+export const signUp = (req, res) => {
+  res.send("Sign Up");
+};
+
+export const signIn = (req, res) => {
+  res.send("Sign In");
+};
+
+export const signOut = (req, res) => {
+  res.send("Sign Out");
+};
